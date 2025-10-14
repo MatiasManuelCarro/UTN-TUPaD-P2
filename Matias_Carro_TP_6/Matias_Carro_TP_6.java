ab11e0aa0ea33b82d3209e46015ed0eff65cfc0b
MatiasManuelCarro <matiasmanuelcarro@gmail.com>
2025-10-13 23:50:19 -0300

    TP 6 parte 1

---
+++ Matias_Carro_TP_6/Matias_Carro_TP_6.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package matias_carro_tp_6;
+
+/**
+ *
+ * @author Matias
+ */
+public class Producto {
+
+    private String id;
+    private String nombre;
+    private double precio;
+    private int cantidad;
+    private CategoriaProducto categoria;
+
+    public Producto(String id, String nombre, double precio, int cantidad, CategoriaProducto categoria) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.categoria = categoria;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public CategoriaProducto getCategoria() {
+        return categoria;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+    
+    
+    
+    
+
+    //public String mostrarInfo();
+    public enum CategoriaProducto {
+        ALIMENTOS("Productos comestibles"),
+        ELECTRONICA("Dispositivos electrónicos"),
+        ROPA("Prendas de vestir"),
+        HOGAR("Artículos para el hogar");
+        private final String descripcion;
+
+        CategoriaProducto(String descripcion) {
+            this.descripcion = descripcion;
+        }
+
+        public String getDescripcion() {
+            return descripcion;
+        }
+    }
+    
+    fdsf
+
+}
