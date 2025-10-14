7e6809b0d028943fed608a4cd02c85303622b2a4
Carro <matias.carro@sofse.gob.ar>
2025-10-14 16:08:03 -0300

    Tp 6 e3 parte 1

---
+++ TP6_punto2/JavaApplication24/src/Tp6_ej3/Curso.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tp6_ej3;
+
+/**
+ *
+ * @author matias.carro
+ */
+public class Curso {
+    
+}
---
+++ TP6_punto2/JavaApplication24/src/Tp6_ej3/Profesor.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tp6_ej3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ *
+ * @author matias.carro
+ */
+public class Profesor {
+
+    private String id;
+    private String nombre;
+    private String especialidad;
+    private List<Curso> cursos = new ArrayList<>();
+    
+    
+        
+}
