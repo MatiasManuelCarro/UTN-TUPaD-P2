0083b3c96412e9c1495a3b3db8ed033a81e120ff
MatiasManuelCarro <matiasmanuelcarro@gmail.com>
2025-10-21 01:14:09 -0300

    empezando tp 7

---
+++ Matias_Carro_TP_7/Matias_Carro_TP_7/src/Matias_carro_TP7_Ej1/Auto.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Matias_carro_TP7_Ej1;
+
+/**
+ *
+ * @author Matias
+ */
+public class Auto extends Vehiculo {
+
+    public Auto(String marca, String modelo) {
+        super(marca, modelo);
+    }
+    
+    
+    
+}
